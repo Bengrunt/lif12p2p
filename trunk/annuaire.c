e1ac4b39fc2f479f5413c6ef788b19536b7217c3
bengrunt <bengrunt@78986ac0-098f-11de-85a1-fd18384f7567>
2009-03-20 13:39:54 +0000

    corrections mineures et réadaptation des includes

---
+++ trunk/annuaire.c
@@ -0,0 +1,23 @@
+/**
+ * @file: annuaire.c
+ * @project: lif12p2p
+ * @author: Rémi AUDUON, Thibault BONNET-JACQUEMET, Benjamin GUILLON
+ * @since: 20/03/2009
+ * @version: 20/03/2009
+ */
+
+ #include "annuaire.h"
+
+
+
+ int main()
+ {
+
+
+    return 0;
+ }
+
+ /*****************
+* Fin de Fichier
+*****************/
+
---
+++ trunk/client_serveur.c
@@ -0,0 +1,22 @@
+/**
+ * @file: client_serveur.c
+ * @project: lif12p2p
+ * @author: Rémi AUDUON, Thibault BONNET-JACQUEMET, Benjamin GUILLON
+ * @since: 20/03/2009
+ * @version: 20/03/2009
+ */
+
+ #include "client_serveur.h"
+
+
+
+ int main()
+ {
+
+
+    return 0;
+ }
+
+ /*****************
+* Fin de Fichier
+*****************/
