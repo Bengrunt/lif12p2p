75c8427235daebfa9b51a05e6b901dfd8976df4d
bengrunt <bengrunt@78986ac0-098f-11de-85a1-fd18384f7567>
2009-03-24 10:18:20 +0000

    modifs annuaire : init close annuaire

--- trunk/annuaire.c
+++ trunk/annuaire.c
@@ -9,25 +9,18 @@
 #include "annuaire.h"
 
 /**
-* Déclaration des variables globales
-*/
-
-BddServeurs* vg_serveurs; /* pointeur sur la liste des serveurs connus */
-BddFichiers* vg_fichiers; /* pointeur sur la liste des fichiers connus */
-
-/**
 * Fonctions et procédures du module
 */
 
-void initialisationAnnuaire()
+void initialisationAnnuaire(BddServeurs * serveurs, BddFichiers * fichiers)
 /**
 * @note: procédure d'initialisation de l'annuaire :  les listes de clients, de serveurs, de fichiers.
 */
 {
 /* Creation des listes de serveurs */
-    vg_serveurs = malloc(sizeof(BddServeurs));
+    serveurs = malloc(sizeof(BddServeurs));
 /* Creation des listes de fichiers */
-    vg_fichiers = malloc(sizeof(BddFichiers));
+    fichiers = malloc(sizeof(BddFichiers));
 }
 
 
@@ -50,20 +43,57 @@ Socket initialiseSocketEcouteAnnuaire(int portAnnuaire)
 }
 
 
-void fermetureAnnuaire()
+void fermetureAnnuaire(BddServeurs * serveurs, BddFichiers * fichiers)
 /**
 * @note: procédure de fermeture de l'annuaire de façon propre.
 */
 {
+/* Variables locales */
+    int i,j;
+    Fichier* temp_fichier;
+    Serveur* temp_serveur;
 /* Destruction des listes de serveurs */
-    free(vg_serveurs);
+    for(j=serveurs->nbServeurs;j>0;j--)
+    {
+        free(serveurs->tabServeurs[j]->adresseServeur);
+        free(serveurs->tabServeurs[j]);
+    }
+    free(serveurs->tabServeurs);
+    free(serveurs);
 /* Destruction des listes de fichiers */
-    free(vg_fichiers);
+    while(fichiers->listeFichiers!=NULL)
+    {
+        for(i=fichiers->listeFichiers->nbBlocs;i>0;i--)
+        {
+            while((fichiers->listeFichiers->tabBlocs[i]).listeServeurs!=NULL)
+            {
+                temp_serveur=(fichiers->listeFichiers->tabBlocs[i]).listeServeurs;
+                (fichiers->listeFichiers->tabBlocs[i]).listeServeurs=temp_serveur->serveurSuivant;
+                free(temp_serveur);
+            }
+        }
+        free(fichiers->listeFichiers->tabBlocs);
+        free(fichiers->listeFichiers->nomFichier);
+
+        temp_fichier=fichiers->listeFichiers;
+        fichiers->listeFichiers=temp_fichier->fichierSuivant;
+        free(temp_fichier);
+    }
+    free(fichiers);
 }
 
 
 int main()
 {
+    /* Initialisation de l'annuaire */
+    BddServeurs* serveurs; /* pointeur sur la liste des serveurs connus */
+    BddFichiers* fichiers; /* pointeur sur la liste des fichiers connus */
+    initialisationAnnuaire(serveurs, fichiers);
+
+
+
+    /* Fermeture de l'annuaire */
+    fermetureAnnuaire(serveurs, fichiers);
 
 
     return 0;
