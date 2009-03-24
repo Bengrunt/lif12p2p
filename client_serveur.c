5977f4d4e60ead6f2e71b8e9bb31b569f6593d05
remaud41@hotmail.com <remaud41@hotmail.com@78986ac0-098f-11de-85a1-fd18384f7567>
2009-03-24 15:02:48 +0000

    modif client serveur

--- client_serveur.c
+++ client_serveur.c
@@ -7,6 +7,8 @@
  */
 
 #include "client_serveur.h"
+#define NBTHREAD 10
+#define TAILLE_BUFF 100
 
 int main()
 {
@@ -38,16 +40,72 @@ void applicationServeur()
 {
     /* déclatation des variables */
     pthread_t variableThread;
+    pthread_t tabThread[NBTHREAD];
     FileAttenteClients listeAttenteClient;
+    int i;
 
     /* initialisation de la liste d'attente */
     initialisationListeAttenteClient(listeAttenteClient);
 
+    /* signalisation des fichiers disponibles à l'annuaire*/
+    signalisationFichierAnnuaire();
+
     /* création du thread pour écouter les demandes clients */
-    pthread_create(variableThread, NULL, (void*) threadDialogueClient, (void*) listeAttenteClient);
+    pthread_create(variableThread, NULL, (void*) threadDialogueClient, (void*) &listeAttenteClient);
+
+    /* création des threads pour envoyer des blocs */
+    i = 0;
+    while (i < NBTHREAD)
+    {
+        pthread_create(tabThread[i], NULL, (void*) threadEnvoiMessage, (void*) &listeAttenteClient);
+        i++;
+    }
+
+    /* surveillance du clavier pour détecter s'il y a une frappe */
+    while (1)
+    {
+        char buff[TAILLE_BUFF];
+        fgets(buff, TAILLE_BUFF, stdin);
+        /* Le dernier carractère est un retour chariot */
+        buff[strlen(buff)-1] = '\0';
+
+        if (strcmp(buff, "fin serveur") == 0)
+        {
+            break;
+        }
+    }
+
+    /* arrêt du serveur */
+        /* arrêt de tous les threads lancés */
+    /***************************************/
+        /* suppression de la liste d'attente */
+    arretServeur();
 
 
+}
 
+void signalisationFichierAnnuaire()
+{
+
+}
+
+void initialisationListeAttenteClient(FileAttenteClients listeAttente)
+{
+
+}
+
+void threadDialogueClient(FileAttenteClients* listeAttente)
+{
+
+}
+
+void threadEnvoiMessage(FileAttenteClients* listeAttente)
+{
+
+}
+
+void arretServeur()
+{
 
 }
 
