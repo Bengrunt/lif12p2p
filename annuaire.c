40bbbf79c6d91d369d9d178cf2d4c3ee9b785757
bengrunt <bengrunt@78986ac0-098f-11de-85a1-fd18384f7567>
2009-03-25 15:56:09 +0000

    modifs annuaire

--- annuaire.c
+++ annuaire.c
@@ -3,7 +3,7 @@
  * @project: lif12p2p
  * @author: Rémi AUDUON, Thibault BONNET-JACQUEMET, Benjamin GUILLON
  * @since: 20/03/2009
- * @version: 24/03/2009
+ * @version: 25/03/2009
  */
 
 #include "annuaire.h"
@@ -17,6 +17,7 @@ int initialisationAnnuaire(BddServeurs * serveurs, BddFichiers * fichiers)
 * @note: procédure d'initialisation de l'annuaire :  les listes de clients, de serveurs, de fichiers.
 * @param: serveurs : pointeur sur la base de données des serveurs.
 * @param: fichiers : pointeur sur la base de données des fichiers.
+* @return: renvoie 0 si tout se passe bien, -1 sinon.
 */
 {
 /* Creation des listes de serveurs */
@@ -42,6 +43,7 @@ Socket initialiseSocketEcouteAnnuaire(int portAnnuaire)
 /**
 * @note: fonction d'initialisation de la socket d'écoute de l'annuaire.
 * @param: portAnnuaire : numéro de port sur lequel on crée la socket d'écoute.
+* @return: renvoie la socket créée.
 */
 {
 /* On déclare la socket pour l'annuaire */
@@ -54,6 +56,104 @@ Socket initialiseSocketEcouteAnnuaire(int portAnnuaire)
     definitionNomSocket(socketEcouteAnnuaire,portAnnuaire);
 
     return socketEcouteAnnuaire;
+}
+
+
+int traiteMessage(Socket arg)
+/**
+* @note: fonction globale de traitement d'un message reçu.
+* @param: arg : socket sur laquelle le message est arrivé.
+* @return: renvoir 0 si tout se passe bien, -1 sinon.
+*/
+{
+/* variables */
+    char* buff; /* Message lu sur la socket */
+    int fin_thread; /* booléen qui détermine si le thread doit se finir ou pas */
+
+/* boucle de traitement des messages */
+    fin_thread=1;
+    while (!fin_thread)
+    {
+/* on ecoute sur la socket arg */
+        buff=ecouteSocket(arg);
+
+/* on analyse le type du message reçu et on agit en conséquence */
+        switch (buff[0])
+        {
+            case '0':
+                switch (buff[1])
+                {
+                    case '3': /* Demande d'un fichier */
+                        traiteDemandeFichierClient();
+                        break;
+                    case '4': /* Demande d'un bloc */
+                        traiteDemandeBlocClient();
+                        break;
+                    case '6': /* Disponibilité d'un bloc */
+                        traiteBlocDisponibleServeur();
+                        break;
+                    case '7': /* Arret d'un serveur */
+                        traiteArretServeur();
+                        break;
+                    default:
+                        traiteMessageErr();
+                }
+
+            default:
+                traiteMessageErr();
+        }
+    }
+
+    return 0;
+
+}
+
+
+void traiteDemandeFichierClient()
+/**
+* @note: traitement d'un message de type demande de fichier d'un client.
+* @param:
+*/
+{
+
+}
+
+
+void traiteDemandeBlocClient()
+/**
+* @note: traitement d'un message de type demande de bloc d'un client.
+* @param:
+*/
+{
+
+}
+
+
+void traiteBlocDisponibleServeur()
+/**
+* @note: traitement d'un message de type nouveau bloc.
+* @param:
+*/
+{
+
+}
+
+void traiteArretServeur()
+/**
+* @note: traitement d'un message de type arrêt de serveur.
+* @param:
+*/
+{
+
+}
+
+
+void traiteMessageErr()
+/**
+* @note: traitement d'un message non géré par traiteMessage().
+*/
+{
+
 }
 
 
@@ -103,11 +203,21 @@ int main(void)
 {
 /* Variables */
     int portEcouteAnnuaire; /* port d'écoute de l'annuaire */
-    int quitter; /* Booléen pour savoir si on quitte ou pas */
+    int nbthreads; /* nombres de threads clients gérés actuellement */
+
+    Socket socketEcouteAnnuaire; /* Socket d'écoute de l'annuaire */
+    Socket socketDemandeConnexion; /* Socket ouverte par un client */
+
     BddServeurs* serveurs; /* pointeur sur la liste des serveurs connus */
     BddFichiers* fichiers; /* pointeur sur la liste des fichiers connus */
+
+    int quitter; /* Booléen pour savoir si on quitte ou pas */
     int numport_ok; /* booleen pour l'entrée du port annuaire */
+
     char numport_key; /* confirmation de réponse pour le port annuaire */
+
+    pthread_t* th_client; /* tableau de threads pour gerer les connexions clients */
+
 
 /* Initialisation de l'annuaire */
     printf("Bienvenue sur le programme lif12p2p.\n");
@@ -136,13 +246,17 @@ int main(void)
 
     }while(numport_ok);
 
-    initialiseSocketEcouteAnnuaire(portEcouteAnnuaire);
+    socketEcouteAnnuaire=initialiseSocketEcouteAnnuaire(portEcouteAnnuaire);
 
 /* Menu */
     quitter=1;
+    nbthreads=0;
     while(quitter)
     {
+        socketDemandeConnexion=acceptationConnexion(socketEcouteAnnuaire);
 
+        pthread_create(&th_client[nbthreads+1], NULL, traiteMessage , (void*)socketDemandeConnexion );
+        nbthreads++;
     }
 
 /* Fermeture de l'annuaire */
